9b1e21bef6c5d7a57bd6c7c12074661817879d8e
Jedi Fonseca <jedifn7@gmail.com>
2025-10-20 18:45:01 -0300

    Update windows-11-post-install.ps1

--- windows-11-post-install.ps1
+++ windows-11-post-install.ps1
@@ -33,6 +33,7 @@ $apps = @(
     "qBittorrent.qBittorrent",
     "Hydrogen-Music.Hydrogen",
     "Baidu.TeraBox",
+    "Apple.iTunes",
 
     # Proton Apps
     "Proton.ProtonPass",
@@ -74,7 +75,7 @@ Write-Host ""
 Write-Host "Ao ser executado, este script irá:" -ForegroundColor Yellow
 Write-Host ""
 Write-Host "1. Instalar, com o winget, os pacotes: Steam, Epic Games Launcher, EA App, Ubisoft Connect,"
-Write-Host "   GOG Galaxy, Notesnook, Remote Mouse, VLC, Firefox, Audacity, Localsend, 7zip, Gimp,"
+Write-Host "   GOG Galaxy, Notesnook, Remote Mouse, VLC, iTunes, Firefox, Audacity, Localsend, 7zip, Gimp,"
 Write-Host "   Upscayl, qBittorrent, Hydrogen, Terabox, Proton Pass e Proton Authenticator."
 Write-Host "2. Instalar os Microsoft Virtual C++ Redistributable de 32 e 64 bits nas versões de"
 Write-Host "   2005, 2008, 2010, 2012, 2013 e 2015+."
@@ -137,6 +138,7 @@ Write-Host "ou CTRL+C para não abrir a página e encerrar a execução do scrip
 Read-Host
 Start-Process "https://www.amd.com/pt/support/downloads/drivers.html/graphics/radeon-600-500-400/radeon-rx-500-series/radeon-rx-580.html"
 Write-Host "Você chegou ao final do script." -ForegroundColor Green
+
 
 
 
