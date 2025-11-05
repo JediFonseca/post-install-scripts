6657663624c26e512b52e897b92577cdc3d2f0e5
Jedi Fonseca <jedifn7@gmail.com>
2025-11-05 19:01:47 -0300

    Update windows-11-post-install.ps1

--- windows-11-post-install.ps1
+++ windows-11-post-install.ps1
@@ -61,6 +61,7 @@ $downloads = @(
     "https://www.foobar2000.org/getcomponent/e49c218fdddd549e5e428dc63acb8aff/foo_openlyrics-v1.12.fb2k-component",
     "https://www.oo-software.com/en/download/current/ooshutup10",
     "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe",
+    "https://github.com/cmdr-sheppard/strawberry/releases/download/1.2.10/StrawberrySetup-1.2.10.8.g04c6c862-msvc-x64.exe",
     "https://www.x360ce.com/files/x360ce.zip"
 )
 
@@ -83,7 +84,8 @@ Write-Host "   GOG Galaxy, Simplenote, Remote Mouse, VLC, iTunes, Firefox, Audac
 Write-Host "   Upscayl, qBittorrent, Hydrogen, Terabox, Proton Pass e Proton Authenticator."
 Write-Host "2. Instalar os Microsoft Virtual C++ Redistributable de 32 e 64 bits nas versões de"
 Write-Host "   2005, 2008, 2010, 2012, 2013 e 2015+."
-Write-Host "3. Baixar os executáveis dos apps: Rockstar Launcher, Media Downloader, O&O ShutUp10++, foobar2000 e x360ce."
+Write-Host "3. Baixar os executáveis dos apps: Rockstar Launcher, Media Downloader, O&O ShutUp10++,"
+Write-Host "   Strawberry Music Player, foobar2000 e x360ce."
 Write-Host "4. Baixar os plugins para o foobar2000: foo_scrobble e OpenLyrics."
 Write-Host "5. Habilitar o menu de contexto clássico e reiniciar o processo explorer.exe."
 Write-Host "6. (Opcional) Abrir o navegador padrão na página de download do driver da RX580."
@@ -154,4 +156,5 @@ Write-Host "ou CTRL+C para não abrir a página e encerrar a execução do scrip
 Read-Host
 Start-Process "https://www.amd.com/pt/support/downloads/drivers.html/graphics/radeon-600-500-400/radeon-rx-500-series/radeon-rx-580.html"
 Write-Host "Você chegou ao final do script." -ForegroundColor Green
+
 
