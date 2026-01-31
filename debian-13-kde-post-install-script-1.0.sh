e1b8529cb341548591c8c62e83029551a092dd2c
Jedi Fonseca <jedifn7@gmail.com>
2026-01-31 02:25:06 -0300

    Add files via upload

---
+++ debian-13-kde-post-install-script-1.0.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------
+# Script de pós instalação para Debian 13 ---------------------------
+# Autor: Jedielson da Fonseca----------------------------------------
+#--------------------------------------------------------------------
+
+#-------------------------------------------------
+# ---Pacotes para instalação, downloads e cores---
+#-------------------------------------------------
+
+apt_packages=(
+    "tree"
+    "mangohud"
+    "gamemode"
+    "vlc"
+    "linux-cpupower"
+    "soundconverter"
+    "audacity"
+    "xboxdrv"
+    "pipewire-audio-client-libraries"
+    "rclone"
+    "ratbagd"
+    "piper"
+)
+
+flatpak_packages=(
+    "com.usebottles.bottles"
+    "com.github.finefindus.eyedropper"
+    "com.github.tchx84.Flatseal"
+    "org.localsend.localsend_app"
+    "io.github.peazip.PeaZip"
+    "com.vysp3r.ProtonPlus"
+    "com.protonvpn.www"
+    "org.gimp.GIMP"
+    "org.upscayl.Upscayl"
+    "com.markopejic.downloader"
+    "org.bunkus.mkvtoolnix-gui"
+    "org.freedesktop.Platform.VulkanLayer.MangoHud"
+    "com.github.Flacon"
+    "org.qbittorrent.qBittorrent"
+    "org.gnome.Boxes"
+    "org.strawberrymusicplayer.strawberry"
+    "com.valvesoftware.Steam"
+    "org.hydrogenmusic.Hydrogen"
+    "com.heroicgameslauncher.hgl"
+    "com.github.Matoking.protontricks"
+    "com.brave.Browser"
+    "net.cozic.joplin_desktop"
+)
+
+snap_packages=(
+    "copilot-desktop"
+)
+
+deb_downloads=(
+    "https://proton.me/download/authenticator/linux/ProtonAuthenticator_1.1.4_amd64.deb"
+    "https://proton.me/download/pass/linux/proton-pass_1.33.5_amd64.deb"
+    "https://data.nephobox.com/issue/terabox/Linux/1.42.6/TeraBox_1.42.6_amd64.deb"
+    "https://github.com/TheAssassin/AppImageLauncher/releases/download/v3.0.0-beta-3/appimagelauncher_3.0.0-beta-2-gha287.96cb937_amd64.deb"
+)
+
+appimages_downloads=(
+    "https://github.com/JediFonseca/mass_renamer/releases/download/mass_renamer-2.3.1-bugfix/mass_renamer-2.3.1-x86_64.AppImage"
+)
+
+# Paleta de cores
+HEADER='\033[0;36m'  # Ciano - para títulos.
+SUCCESS='\033[0;32m' # Verde - para mensagens de sucesso.
+INFO='\033[1;33m'   # Amarelo - para avisos.
+NOCOLOR='\033[0m' # Reseta a cor para o padrão do terminal.
+ERRORS='\033[0;31m' # Erros
+
+#--------------------------------
+# ---FASE 0 - Mensagem inicial---
+#--------------------------------
+
+echo -e "${HEADER}##############################################${NOCOLOR}"
+echo -e "${HEADER}##   Script de pós instalação do Debian 13  ##${NOCOLOR}"
+echo -e "${HEADER}##############################################${NOCOLOR}"
+echo
+echo -e "${ERRORS}VERIFIQUE OS LINKS DE DOWNLOAD ANTES DE USAR${NOCOLOR}"
+echo
+echo -e "${INFO}Ao ser executado, este script irá:${NOCOLOR}"
+echo -e "1. Instalar o pacote \"flatpak\" e adicionar o repositório \"Flathub\"."
+echo -e "2. Instalar, dos repositórios do Debian, os pacotes: tree, mangohud, gamemode,"
+echo -e "   vlc, soundconverter, audacity, xboxdrv, pipewire-audio-client-libraries,"
+echo -e "   rclone, ratbagd e piper."
+echo -e "3. Instalar os flatpaks: Bottles, Eyedropper, Flatseal, LocalSend, PeaZip, ProtonPlus,"
+echo -e "   Proton VPN, GIMP, Upscayl, Video Downloader, MKVToolNix, MangoHud, Flacon,"
+echo -e "   qBittorrent, GNOME Boxes, Strawberry, Steam, Hydrogen, Heroic Games Launcher,"
+echo -e "   Protontricks, Brave e Joplin."
+echo -e "5. Baixar, no formato .deb, os instaladores dos apps: Proton Pass, TeraBox,"
+echo -e "   Proton Authenticator e AppImageLauncher."
+echo -e "6. Baixar, em AppImage, os apps: Mass Renamer."
+echo -e "7. Instalar os pacotes .deb baixados."
+echo -e "8. Ativar o modo de energia \"performance\" para a CPU."
+echo -e "9. Desinstalar os pacotes: kaddressbook, pim-sieve-editor, korganizer, juk, kmouth,"
+echo -e "   libkdsoapwsdiscoveryclient0:amd64, plasma-discover, dragonplayer e konqueror."
+echo
+echo -e -n "${INFO}Pressione ENTER para iniciar instalação das dependências ou CTRL+C para cancelar.${NOCOLOR}"
+read
+echo
+
+#-------------------------------------------------
+# ---FASE 1 - Resolvendo dependências do script---
+#-------------------------------------------------
+
+echo -e "${INFO}Instalando o pacote \"flatpak\" e adicionando o repositório \"Flathub\".${NOCOLOR}"
+echo
+sudo apt update
+sudo apt install -y flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+echo
+echo -e "${INFO}Fase de instalação de dependências finalizada.${NOCOLOR}"
+
+echo
+echo -e -n "${INFO}Pressione ENTER para instalar os pacotes \"apt\", flatpaks e snaps ou CTRL+C para cancelar.${NOCOLOR}"
+read
+echo
+
+#------------------------------------------------------------------------------
+# ---FASE 2 - Instalando pacotes dos repositórios oficiais, flatpaks e snaps---
+#------------------------------------------------------------------------------
+
+# Instalando pacotes do APT:
+echo -e "${INFO}Iniciando instalação dos pacotes \"apt\".${NOCOLOR}"
+echo
+sudo apt install -y "${apt_packages[@]}"
+    if [ $? -ne 0 ]; then
+        echo -e "${ERRORS}A instalação de um ou mais pacotes falhou. Verifique os nomes dos pacotes.${NOCOLOR}"
+        echo -e "${INFO}Tentando corrigir...${NOCOLOR}"
+        sudo apt --fix-broken install -y
+        echo -e "${INFO}Repetindo a instalação dos pacotes...${NOCOLOR}"
+        sudo apt install -y "${apt_packages[@]}"
+            if [ $? -ne 0 ]; then
+                echo -e "${ERRORS}A instalação de um pacote .deb ou mais falhou novamente.${NOCOLOR}"
+                echo -e "${INFO}Continuando com a execução do script...${NOCOLOR}"
+            else
+                echo -e "${SUCCESS}Segunda tentativa: pacotes instalados com sucesso.${NOCOLOR}"
+            fi
+    else
+        echo -e "${SUCCESS}Pacotes instalados com sucesso.${NOCOLOR}"
+    fi
+
+# Instalação dos pacotes flatpak:
+echo -e "${INFO}Iniciando a instalação dos pacotes flatpak${NOCOLOR}"
+echo
+flatpak install -y flathub "${flatpak_packages[@]}"
+    if [ $? -ne 0 ]; then
+        echo -e "${ERRORS}A instalação de um ou mais flatpaks falhou. Verifique as IDs dos aplicativos.${NOCOLOR}"
+    else
+        echo -e "${SUCCESS}Flatpaks instalados com sucesso.${NOCOLOR}"
+    fi
+
+# Instalação dos pacotes snap:
+#echo -e "${INFO}Iniciando a instalação dos pacotes snap.${NOCOLOR}"
+#echo
+#sudo snap wait system seed.loaded
+#sudo snap refresh
+#sudo snap install "${snap_packages[@]}"
+#    if [ $? -ne 0 ]; then
+#        echo -e "${ERRORS}A instalação de um ou mais snaps falhou. Verifique os nomes dos pacotes.${NOCOLOR}"
+#    else
+#        echo -e "${SUCCESS}Snaps instalados com sucesso.${NOCOLOR}"
+#    fi
+echo
+echo -e "${INFO}Fase de instalação dos pacotes finalizada.${NOCOLOR}"
+
+echo
+echo -e -n "${INFO}Pressione ENTER para baixar os arquivos .deb ou CTRL+C para cancelar.${NOCOLOR}"
+read
+echo
+
+#-----------------------------------------------------
+# ---FASE 3 Downloads dos arquivos .deb e .appimage---
+#-----------------------------------------------------
+
+echo -e "${INFO}Iniciando o download dos pacotes .deb...${NOCOLOR}"
+echo
+mkdir -p "$HOME/Downloads/Debs"
+wget --show-progress -P "$HOME/Downloads/Debs" "${deb_downloads[@]}"
+
+echo -e "${INFO}Iniciando o download dos pacotes AppImage...${NOCOLOR}"
+echo
+mkdir -p "$HOME/Downloads/AppImages"
+wget --show-progress -P "$HOME/Downloads/AppImages" "${appimages_downloads[@]}"
+echo
+echo -e "${INFO}Tornando os AppImages executáveis...${NOCOLOR}"
+
+echo
+chmod +x "$HOME/Downloads/AppImages/"*.AppImage
+
+echo
+echo -e "${INFO}Fase de Downloads finalizada.${NOCOLOR}"
+echo
+
+echo -e -n "${INFO}Pressione ENTER para instalar os pacotes .deb ou CTRL+C para cancelar.${NOCOLOR}"
+read
+echo
+
+#------------------------------------------
+# ---FASE 4 Instalação dos arquivos .deb---
+#------------------------------------------
+
+echo -e "${INFO}Iniciando a instalação dos pacotes .deb.${NOCOLOR}"
+echo
+sudo apt install -y "$HOME/Downloads/Debs/"*.deb
+    if [ $? -ne 0 ]; then
+        echo -e "${ERRORS}A instalação de um pacote .deb ou mais falhou. Tentando corrigir...${NOCOLOR}"
+        sudo apt --fix-broken install -y
+        echo -e "${INFO}Repetindo a instalação dos pacotes .deb...${NOCOLOR}"
+        sudo apt install -y "$HOME/Downloads/Debs/"*.deb
+            if [ $? -ne 0 ]; then
+        	echo -e "${ERRORS}A instalação de um pacote .deb ou mais falhou novamente.${NOCOLOR}"
+        	echo -e "${INFO}Continuando com a execução do script...${NOCOLOR}"
+            else
+        	echo -e "${SUCCESS}Segunda tentativa: pacotes .deb instalados com sucesso.${NOCOLOR}"
+            fi
+    else
+        echo -e "${SUCCESS}Pacotes .deb instalados com sucesso.${NOCOLOR}"
+    fi
+
+#---------------------------------------------------------------------
+# ---FASE 5 Selecionando o modo de energia "performance" para a CPU---
+#---------------------------------------------------------------------
+
+echo -e "${INFO}Pressione ENTER para identificar o modo de energia atual${NOCOLOR}"
+echo -e -n "${INFO}da sua CPU ou CTRL+C para cancelar...${NOCOLOR}"
+read
+echo
+
+# Identificando o modo atual da CPU.
+echo -e "${INFO}Verificando o modo de energia atual da sua CPU...${NOCOLOR}"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+cpupower frequency-info
+echo -e "${INFO}Verifique, acima, o modo de energia atual da sua CPU.${NOCOLOR}"
+echo -e -n "${INFO}Pressione ENTER para ativar o modo \"performance\" ou CTRL+C para cancelar.${NOCOLOR}"
+read
+
+# Selecionando o modo "performance".
+sudo cpupower frequency-set -g performance
+echo -e "[Unit]\nDescription=Set CPU governor to performance mode\nAfter=cpupower.service\n\n[Service]\nExecStart=/usr/bin/cpupower frequency-set -g performance\nRemainAfterExit=true\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/cpupower-performance.service > /dev/null
+sudo systemctl enable cpupower-performance.service
+sudo systemctl start cpupower-performance.service
+
+# Identificando novamente o modo ativo da CPU.
+echo -e "${INFO}Verificando o modo de energia ativo para a sua CPU...${NOCOLOR}"
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+cpupower frequency-info
+
+#----------------------------
+# ---FASE 5 Ajustes extras---
+#----------------------------
+
+# Instalando o "steam-devices" para complementar a instalação da Steam em flatpak.
+echo -e "${INFO}Instalando o pacote \"steam-devices\" para complementar a instalação da Steam em flatpak.${NOCOLOR}"
+sudo apt install -y steam-devices
+
+# Adicionando permissão para que o mangohud em flatpak possa acessar a partição onde os jogos estão instalados.
+echo -e "${INFO}Adicionando permissão para que o mangohud em flatpak possa acessar a partição com os jogos.${NOCOLOR}"
+echo -e "${INFO}Para que esse ajuste funcione, a partição deve estar montada em \"'/mnt/Dados (Linux)\".${NOCOLOR}"
+echo
+echo -e -n "${INFO}Pressione ENTER para prosseguir ou CTRL+C para encerrar o script.${NOCOLOR}"
+read
+echo -e "${HEADER}33%${NOCOLOR}"
+echo -e "${HEADER}66%${NOCOLOR}"
+flatpak override --user --filesystem='/mnt/Dados (Linux):rw' org.freedesktop.Platform.VulkanLayer.MangoHud
+echo -e "${HEADER}100%${NOCOLOR}"
+
+#-----------------------------------------------
+# ---FASE 6 Remoção de pacotes desnecessários---
+#-----------------------------------------------
+
+echo -e "${INFO}Por fim, o script irá desinstalar os pacotes desnecessários.${NOCOLOR}"
+echo
+echo -e -n "${INFO}Pressione ENTER para prosseguir ou CTRL+C para encerrar o script.${NOCOLOR}"
+read
+echo
+sudo apt purge kmail kaddressbook pim-sieve-editor korganizer juk kmouth libkdsoapwsdiscoveryclient0:amd64 plasma-discover dragonplayer konqueror -y
+sudo apt autoremove -y
+echo
+echo -e -n "${INFO}Script finalizado. Pressione ENTER para encerrar a execução.${NOCOLOR}"
+read
+echo
