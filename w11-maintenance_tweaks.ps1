cba6af29226ac6d7781024a9a6d081d042073a47
Jedi Fonseca <jedifn7@gmail.com>
2026-02-22 14:40:21 -0300

    Update w11-maintenance_tweaks.ps1

--- w11-maintenance_tweaks.ps1
+++ w11-maintenance_tweaks.ps1
@@ -18,6 +18,20 @@ foreach ($app in $packages) {
     Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq $app} | Remove-AppxProvisionedPackage -AllUsers -Online -ErrorAction SilentlyContinue
 }
 
+# Bloquear reinstalação silenciosa de bloatware
+$cdmPath = "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+$cdmNames = @('ContentDeliveryAllowed','SilentInstalledAppsEnabled','OEMPreInstalledAppsEnabled',
+              'PreInstalledAppsEnabled','SubscribedContentEnabled')
+foreach ($name in $cdmNames) {
+    reg.exe add $cdmPath /v $name /t REG_DWORD /d 0 /f
+}
+
+# Restaurar menu de contexto clássico
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+# Restaurar plano de energia Máximo Desempenho
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+
 # Registros Críticos (Forçados)
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d 0 /f
@@ -26,3 +40,4 @@ reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /
 reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 
 Write-Host "SISTEMA BLINDADO COM SUCESSO!" -ForegroundColor Green
+
