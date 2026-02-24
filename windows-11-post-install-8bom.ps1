19590a0d483ff892756d8d137b4f7599afbe768a
Jedi Fonseca <jedifn7@gmail.com>
2026-02-23 21:15:08 -0300

    Update windows-11-post-install-8bom.ps1

--- windows-11-post-install-8bom.ps1
+++ windows-11-post-install-8bom.ps1
@@ -144,19 +144,6 @@ Read-Host
 Stop-Process -Name explorer -Force
 Start-Process explorer.exe
 
-#--------------------------------------------------------
-# ---Habilitando o modo de energia "Máximo Desempenho"---
-#--------------------------------------------------------
-
-Write-Host "Habilitando o modo de energia Máximo Desempenho..." -ForegroundColor Yellow
-Write-Host "33%" -ForegroundColor Cyan
-Write-Host "66%" -ForegroundColor Cyan
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
-Write-Host "100%" -ForegroundColor Cyan
-Write-Host ""
-Write-Host "Modo de energia Máximo Desempenho habilitado." -ForegroundColor Yellow
-Write-Host ""
-
 # Mensagem final
 
 Write-Host "Script finalizado!" -ForegroundColor Yellow
@@ -165,4 +152,5 @@ Write-Host "ou CTRL+C para não abrir a página e encerrar a execução do scrip
 Read-Host
 Start-Process "https://www.amd.com/pt/support/downloads/drivers.html/graphics/radeon-600-500-400/radeon-rx-500-series/radeon-rx-580.html"
 Write-Host "Você chegou ao final do script." -ForegroundColor Green
+
 
